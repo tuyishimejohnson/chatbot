27965793071eef13b50ac3f3de47e9a8d37bb7ec
tuyishime johnson <j.tuyishime4@alustudent.com>
2025-02-21 17:43:08 +0200

    Change the font family

--- agri-chatbot/tailwind.config.ts
+++ agri-chatbot/tailwind.config.ts
@@ -12,6 +12,10 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+
+      fontFamily: {
+        Nunito_Sans: ["Nunito Sans", "sans-serif"],
+      }
     },
   },
   plugins: [],
