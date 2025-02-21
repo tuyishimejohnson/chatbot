2d82666cf8f89b43140a07e8e46f3438f483b238
tuyishime johnson <j.tuyishime4@alustudent.com>
2025-02-21 18:14:32 +0200

    Add a background color on signin and signup pages

--- agri-chatbot/app/layout.tsx
+++ agri-chatbot/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className="font-Outfit antialiased">
-          <header className="flex justify-end items-center p-4 gap-4 h-16">
+          <header className="flex justify-end items-center py-4 px-14 bg-green-800 gap-4 text-white ">
             <SignedOut>
               <SignInButton />
               <SignUpButton />
