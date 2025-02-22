b97d2e49c4537f3c2623cffcd2983e16efabdcd7
tuyishime johnson <j.tuyishime4@alustudent.com>
2025-02-22 17:27:07 +0200

    Update the structure of the model by cleaning, removing empty values, and tokenization

--- server.py
+++ server.py
@@ -7,8 +7,14 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/api/home/", methods=["GET"])
+@app.route("/api/home/", methods=["GET", "POST"])
 def return_home():
+    return jsonify({
+        "message": "Hello world!!!!!!!!!!!!!!!!!!!!"
+    })
+
+@app.route("/http://localhost:3000/", methods=["POST", "GET"])
+def process():
     return jsonify({
         "message": "Hello world!!!!!!!!!!!!!!!!!!!!"
     })
