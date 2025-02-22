92fd99560d542d73f435dfd317842752e643e04d
tuyishime johnson <j.tuyishime4@alustudent.com>
2025-02-22 12:23:48 +0200

    Change the z-index

--- agri-chatbot/app/layout.tsx
+++ agri-chatbot/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className="font-Outfit antialiased">
-          <header className="flex justify-end items-center py-4 px-14 bg-green-800 gap-4 text-white ">
+          <header className="flex justify-end items-center py-4 px-14 bg-green-800 gap-4 text-white fixed right-0 left-0 -z-10">
             <SignedOut>
               <SignInButton />
               <SignUpButton />
